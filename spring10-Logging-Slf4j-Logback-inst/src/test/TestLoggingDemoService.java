f414cd47cc04f8cdff53c3b0c71fc0ca3a3a5564
VerySimple99 <lyricrain.seo@gmail.com>
2025-07-15 06:56:54 +0900

    feat : di, aop , db 연동

--- spring10-Logging-Slf4j-Logback-inst/src/test/TestLoggingDemoService.java
+++ spring10-Logging-Slf4j-Logback-inst/src/test/TestLoggingDemoService.java
@@ -6,6 +6,6 @@ public class TestLoggingDemoService {
 	public static void main(String[] args) {
 		LoggingDemoService service=new LoggingDemoService();
 		service.testLog();
-		//service.testLog2("월드컵","4강");
+		service.testLog2("월드컵","4강");
 	}
 }
